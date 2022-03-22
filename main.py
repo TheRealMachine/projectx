9b1274e3c356e98bba6f93e8abd93047bf197e42
DaRealMachine <shivam.banerjee@gmail.com>
2022-03-22 09:28:08 +0100

    copied code

---
+++ main.py
@@ -0,0 +1,81 @@
+import enum
+import pygame
+from sprites import *
+from config import *
+import sys
+
+class Game:
+    def __init__(self):
+        pygame.init
+        self.screen = pygame.display.set_mode((win_width, win_height))
+        self.clock = pygame.time.Clock()
+        self.running = True
+  
+    def create_tilemap(self):
+        for i, row in enumerate(tilemap):
+            for j, column in enumerate(row):
+                if column == "B":
+                    Block(self,j,i)
+                if column == "P":
+                    Player(self,j,i)
+                    
+
+
+    def new(self):
+        
+        self.playing = True
+
+        self.all_sprites = pygame.sprite.LayeredUpdates()
+        self.blocks = pygame.sprite.LayeredUpdates()
+        self.enemies = pygame.sprite.LayeredUpdates()
+        self.attacks = pygame.sprite.LayeredUpdates()
+
+        self.create_tilemap()
+
+        
+        
+
+
+
+
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.playing = False
+                self.running = False
+
+    def update(self):
+        self.all_sprites.update()
+
+
+    def draw(self):
+        self.screen.fill(BLACK)
+        self.all_sprites.draw(self.screen)
+        self.clock.tick(fps)
+        pygame.display.update()
+
+    def main(self):
+        # game loop
+        while self.playing:
+            self.events()
+            self.update()
+            self.draw()
+        self.running = False
+
+    def game_over(self):
+        pass
+
+    def intro_screen(self):
+        pass
+
+
+g = Game()
+g.intro_screen()
+g.new()
+while g.running:
+    g.main()
+    g.game_over()
+
+pygame.quit()
+sys.exit()
---
+++ sprites.py
@@ -0,0 +1,75 @@
+import pygame
+from config import *
+import math
+import random
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self,game,x,y):
+
+        self.game = game
+        self._layer = player_layer
+        self.groups = self.game.all_sprites
+        pygame.sprite.Sprite.__init__(self,self.groups)
+
+        self.x = x*tilesize
+        self.y = y*tilesize
+        self.width = tilesize
+        self.height = tilesize
+
+        self.x_change = 0
+        self.y_change = 0
+
+        self.facing = 'down'
+
+        image_to_load = pygame.Surface([self.height, self.width])
+        self.image = pygame.Surface([self.width, self.height])
+        self.image.fill(RED)
+
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+    def update(self):
+        self.movement()
+        
+        self.rect.x += self.x_change
+        self.rect.y += self.y_change
+
+        self.x_change = 0
+        self.y_change = 0
+
+    def movement(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.x_change -= player_speed
+            self.facing = 'left'
+        if keys[pygame.K_RIGHT]:
+            self.x_change += player_speed
+            self.facing = 'right'
+        if keys[pygame.K_UP]:
+            self.y_change -= player_speed
+            self.facing = 'up'
+        if keys[pygame.K_DOWN]:
+            self.y_change += player_speed
+            self.facing = 'down'
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+
+        self.game = game
+        self._layer = block_layer
+        self.groups = self.game.all_sprites, self.game.blocks
+        pygame.sprite.Sprite.__init__(self,  self.groups)
+
+        self.x = x*tilesize
+        self.y = y*tilesize
+        self.width = tilesize
+        self.height = tilesize
+
+        self.image = pygame.Surface([self.width, self.height]) 
+        self.image.fill(BLUE)
+
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y 
